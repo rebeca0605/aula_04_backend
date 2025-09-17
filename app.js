19316a64849690d6668e1779a2c69add57384c8f
REBECA GOMES DA SILVA <25132910@CFP127ALUNO.LOCAL>
2025-09-17 11:23:07 -0300

    Função procurar por sigla quase finalizada

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ const express = require('express') //Responsável pela API
 const cors = require('cors') //Responsável pelas permissões da API (app)
 const bodyParser = require('body-parser') //Responsável por gerenciar a chegada dos dados da API com o Front-End
 
+//Import do arquivo de funções
+const dados = require('./module/funcoes.js')
+
 //Retorna a porta do sevidor atual ou colocamos uma porta local
 const PORT = process.PORT || 8080
 
@@ -30,8 +33,39 @@ app.use((request, response, next) => {
     next() // Próximo, carregar os próximos endpoints
 })
 
+//Request -> chegada de dados na API
+//Response -> retorno de dados na API
+
 //EndPoints
 
 app.get('/v1/estados', function(request, response){
-    
+    //Pesquisa na função de estados 
+    let estados = dados.getAllEstados()
+
+    //Retorna o status code
+    response.status(estados.status_code)
+
+    //retorna o JSON
+    response.json(estados)
+})
+
+app.get('/v1/estado/:uf', function(request, response){
+    let sigla = request.params.uf
+    console.log(sigla)
+})
+
+// app.get('/v1/estados/regiao/:id', function(request, response){
+//     let sigla = request.query.uf
+//     let estado = request.query.estado
+//     let regiao = request.query.regiao
+//     let id = request.params.id
+//     console.log(sigla)
+//     console.log(estado)
+//     console.log(regiao)
+//     console.log(id)
+// })
+
+//Start na API
+app.listen(PORT, function(){
+    console.log('API aguardando requisições...')
 })
--- module/funcoes.js
+++ module/funcoes.js
@@ -5,17 +5,24 @@
  * Versão: 1.0
  ***************************************************************************************************************************************/
 const MESSAGE_ERRO = {status: false, status_code: 500, development: 'Rebeca Gomes.'}
+
+//Import do arquivo de estados e cidades
 const dados = require('./estados_cidades.js')
 
 //Retorna todos os estados
 const getAllEstados = function(){
+    //Variável de base para o cabeçalho da API
     let message = {status: true, status_code: 200, development: 'Rebeca Gomes', uf: []}
 
     dados.listaDeEstados.estados.forEach(function(item){
         message.uf.push(item.sigla)
     })
 
+    //Para adicionar um elemento novo no JSON
     message.quantidade = message.uf.length
+
+    //Para remover um atributo do JSON
+    //delete message.status
 
     if(message.uf.length > 0)
         return message //Saída verdadeira 200
@@ -25,7 +32,17 @@ const getAllEstados = function(){
 
 //Retorna um estado pesquisando pela sigla
 const getEstadoBySigla = function(sigla){
+    //Variável de base para o cabeçalho da API
+    let message = {status: true, status_code: 200, development: 'Rebeca Gomes', }
 
+    let estado = dados.listaDeEstados.estados.find(function(item){
+        return item.sigla.toLowerCase() === sigla.toLowerCase()
+    })
+
+    message.uf = estado.sigla
+    message.nome = estado.nome
+    message.capital = estado.capital
+    message.regiao = estado.regiao
 }
 
 //Retorna a capital referente a um estado pesquisando pela sigla
@@ -47,6 +64,8 @@ const getVerifyCapitaisDoPais = function(){
 const getCidadesBySigla = function(sigla){
 
 }
+
+getEstadoBySigla('sp')
 
 // console.log(getAllEstados())
 
